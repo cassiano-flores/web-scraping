c9a2c40ae205e74c499d8e97f89af82cee246607
cassiano-flores <cassiano.flores2424@gmail.com>
2024-08-25 20:45:34 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Utilizar Regex / BeautifulSoup / Selenium
+
+# ------------------------ TAREFA 1 ------------------------
+# https://www.youtube.com/watch?v=QiyJh-bl-oE
+# 1. Faça um crawler capaz de navegar por todas as páginas de países e acessar seus HTML
+
+# 2. Faça scraping dos HTMLs das páginas para armazenar os seguintes dados dos países em um arquivo CSV:
+ # a. Nome do país (campo country)
+ # b. Nome da moeda (campo currency name)
+ # c. Nome do continente que pertence (campo continente)
+ # d. Nome de todos os países vizinhos (campo neighbours - Atenção, é o NOME e não a sigla!)
+ # e. Salvar uma coluna extra no csv contendo um timestamp do momento no qual os dados foram obtidos.
+
+# 3. Faça um crawler que monitore as páginas de países e procure por atualizações.
+# Caso algum registro tenha sido atualizado desde sua obtenção, esse registro deve ser atualizado
+# no arquivo CSV, caso contrário manter a versão anterior.
+# ----------------------------------------------------------
+
+# ------------------------ TAREFA 2 ------------------------
+# https://www.imdb.com/
+# 1. Faça scraping para obter os filmes presentes no Calendário de Lançamentos do IMDB.
+# Devem ser obtidos: Título, Data de Lançamento, Gênero(s) e o link para página da série.
+
+# 2. Faça scraping das páginas específicas dos filmes obtidos no item anterior.
+# Obtenha dessa página o(s) nome(s) do(s) diretor(es) e a lista dos atores presentes no elenco principal (Não é a lista completa de atores!).
+
+# 3. Salve as informações obtidas em um arquivo de tipo JSON.
+# ----------------------------------------------------------
+
+# ------------------------ TAREFA 3 ------------------------
+# Cada aluno deve elaborar um relatório descrevendo a sua participação no desenvolvimento do trabalho,
+# as tecnologias utilizadas para resolver as tarefas e as facilidades e dificuldades encontradas.
+# Além disso o aluno também deve descrever a sua percepção sobre a participação dos colegas no trabalho.
+# A entrega deve ser em formato de relatório, contendo título e nome do aluno.
+# ----------------------------------------------------------
