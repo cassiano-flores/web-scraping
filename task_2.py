cf04d84d97b22e3277687776089527f2b82ef724
cassiano-flores <cassiano.flores2424@gmail.com>
2024-08-31 14:58:06 -0300

    second task done

--- task_2.py
+++ task_2.py
@@ -1,54 +1,87 @@
 # coding=utf-8
+import io
+import json
 import os
 import requests
-import csv
+from collections import OrderedDict
 from bs4 import BeautifulSoup
-from datetime import datetime
 
 BASE_URL = 'https://www.imdb.com'
 CALENDAR_URL = BASE_URL + '/calendar/'
-MOVIE_URL = BASE_URL + '/title/'
-CSV_FILE = 'movies_data.csv'
-FILE_EXISTS = os.path.isfile(CSV_FILE)
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+JSON_FILE = 'movies_data.json'
+FILE_EXISTS = os.path.isfile(JSON_FILE)
 
 
+def get_movies_data(releases_url):
+    response = requests.get(releases_url, headers=HEADERS)
 
-def get_movie_data():
-    return ''
+    # Procurando o JSON na resposta (simplesmente usando split para extrair)
+    json_start = response.text.find('{"props"')
+    json_end = response.text.find('</script>', json_start)
+    json_data = response.text[json_start:json_end]
+
+    # Carregando o JSON em um objeto Python
+    data = json.loads(json_data)
+
+    # Navegando pelo JSON para extrair os dados dos filmes
+    movies_data = []
+    for group in data['props']['pageProps']['groups']:
+        for entry in group['entries']:
+            movie = {
+                "title": entry['titleText'],
+                "genres": entry['genres'],
+                "release_date": entry['releaseDate'],
+                "link": "https://www.imdb.com/title/" + entry['id']
+            }
+            movie_details = get_movie_details(movie['link'])
+            movie.update(movie_details)
+
+            # Adiciona o dicionário atualizado à lista de filmes
+            movies_data.append(movie)
+
+    key_order = ['title', 'release_date', 'link', 'genres', 'directors', 'cast']
+    ordered_movies_data = [
+        OrderedDict((key, movie[key]) for key in key_order)
+        for movie in movies_data
+    ]
+
+    return ordered_movies_data
 
 
+def get_movie_details(movie_url):
+    response = requests.get(movie_url, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
+    # Extrai o(s) nome(s) do(s) diretor(es)
+    directors = []
+    for director in soup.find_all('a', href=lambda href: href and 'tt_ov_dr' in href):
+        directors.append(director.text.strip())
+
+    # Extrai os principais atores (cast)
+    cast = []
+    for actor in soup.find_all('a', href=lambda href: href and 'tt_cl_t' in href)[:5]:
+        cast.append(actor.text.strip())
+
+    directors = list(set(directors))
+    cast = list(set(cast))
+
+    return {
+        'directors': directors,
+        'cast': cast
+    }
+
+
+def write_json(data):
+    with io.open(JSON_FILE, 'w', encoding='utf-8') as file:
+        file.write(unicode(json.dumps(data, ensure_ascii=False, indent=4)))
 
 
 def main():
-    index = 0
-    # while True:
-    #     index_url = INDEX_URL + str(index)
-    #     print('Navegando para o índice: ' + index_url)
-    #     if process_country_pages(index_url):
-    #         write_csv()
-    #         break
-    #     else:
-    #         index += 1
+    movies_data = get_movies_data(CALENDAR_URL)
+    write_json(movies_data)
 
 
 if __name__ == "__main__":
     main()
-
-# ------------------------ TAREFA 2 ------------------------
-# https://www.imdb.com/
-# 1. Faça scraping para obter os filmes presentes no Calendário de Lançamentos do IMDB.
-# Devem ser obtidos: Título, Data de Lançamento, Gênero(s) e o link para página da série.
-
-# 2. Faça scraping das páginas específicas dos filmes obtidos no item anterior.
-# Obtenha dessa página o(s) nome(s) do(s) diretor(es) e a lista dos atores presentes no elenco principal (Não é a lista completa de atores!).
-
-# 3. Salve as informações obtidas em um arquivo de tipo JSON.
-# ----------------------------------------------------------
-
-# ------------------------ TAREFA 3 ------------------------
-# Cada aluno deve elaborar um relatório descrevendo a sua participação no desenvolvimento do trabalho,
-# as tecnologias utilizadas para resolver as tarefas e as facilidades e dificuldades encontradas.
-# Além disso o aluno também deve descrever a sua percepção sobre a participação dos colegas no trabalho.
-# A entrega deve ser em formato de relatório, contendo título e nome do aluno.
-# ----------------------------------------------------------
